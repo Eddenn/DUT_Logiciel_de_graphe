7eb138337a5a58b610fda17046ee57913a81fdf1
Alexandre Hazard <hazard.alexandre76@gmail.com>
2016-01-12 15:02:37 +0100

    no message

--- src/view/HCI.java
+++ src/view/HCI.java
@@ -305,37 +305,11 @@ public class HCI extends JFrame implements ActionListener, ListSelectionListener
 		buttonUndo = initSmoothButton("Défaire","/undo.png","/undo_rollover.png");
 		pTopButton.add(buttonUndo);
 		// Redo
-<<<<<<< HEAD
 		buttonRedo = initSmoothButton("Refaire","/redo.png","/redo_rollover.png");
 		pTopButton.add(buttonRedo);
 		// Parametre
 		buttonSetting = initSmoothButton("Paramètres","/parametre.png","/parametre_rollover.png");
 		pTopButton.add(buttonSetting);
-=======
-		buttonRedo = new JButton(new ImageIcon("images/redo.png"));
-		buttonRedo.setContentAreaFilled(false);
-		buttonRedo.setBorderPainted(false);
-		buttonRedo.setRolloverIcon(new ImageIcon("images/redo_rollover.png"));
-		buttonRedo.setMargin(new Insets(0, 0, 0, 0));
-		buttonRedo.setBackground(new Color(255,255,255));
-		buttonRedo.setForeground(new Color(255,255,255));
-		buttonRedo.setToolTipText("Redo");
-		buttonRedo.addActionListener(this);
-		pButton.add(buttonRedo);
-		
-
-		// Settings
-		buttonSetting = new JButton(new ImageIcon("images/parametre.png"));
-		buttonSetting.setContentAreaFilled(false);
-		buttonSetting.setBorderPainted(false);
-		buttonSetting.setRolloverIcon(new ImageIcon("images/parametre_rollover.png"));
-		buttonSetting.setMargin(new Insets(0, 0, 0, 0));
-		buttonSetting.setBackground(new Color(255,255,255));
-		buttonSetting.setForeground(new Color(255,255,255));
-		buttonSetting.setToolTipText("Paramètres");
-		buttonSetting.addActionListener(this);
-		pButton.add(buttonSetting);
->>>>>>> 9ec5b5a73a0477af4bf3bd9d5c8cc91361ee17f4
 
 		pButton.add(pTopButton);
 		
