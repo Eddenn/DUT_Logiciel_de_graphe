c16767d865cc2100c01da0b16f21f9dfd33242b7
MagicTruite <mehdi.alouache@etu.univ-lehavre.fr>
2016-01-13 10:21:16 +0100

    Fixed une dizaine de warnings

--- src/view/PopupAddVertex.java
+++ src/view/PopupAddVertex.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
